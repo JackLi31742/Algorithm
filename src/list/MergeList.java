01fd6fcd0c510a606cefe0d134c8585ed6347ac7
JackLi31742 <317426838@qq.com>
2019-09-01 23:50:46 +0800

    merge k list 有点问题

--- src/list/MergeList.java
+++ src/list/MergeList.java
@@ -25,17 +25,18 @@ public class MergeList {
 			l2=l2.next;
 		}
         ListNode p=newHead;
-        //指针不能再这走
+        //指针不能在循环的条件这走
         while(l1!=null&&l2!=null) {
         	if (l1.val<=l2.val) {
 				p.next=l1;
 				l1=l1.next;
-				p=p.next;
+//				p=p.next;
 			}else {
 				p.next=l2;
 				l2=l2.next;
-				p=p.next;
+//				p=p.next;
 			}
+        	p=p.next;
         }
         if (l1!=null) {
 			p.next=l1;
@@ -52,7 +53,40 @@ public class MergeList {
 	 * @return
 	 */
 	public ListNode mergeKLists(ListNode[] lists) {
-        
+        if (lists==null||lists.length==0) {
+			return null;
+		}
+        if (lists.length==1) {
+			return lists[0];
+		}
+        //哨兵
+        ListNode newHead=new ListNode(Integer.MIN_VALUE);
+        ListNode p=newHead;
+        while(lists.length>0){
+        	int pos=0;
+        	ListNode temp=lists[pos];
+        	while(temp==null){
+        		pos++;
+        		temp=lists[pos];
+        	}
+        	for (int i = 0; i < lists.length; i++) {
+        		if (lists[i].val<temp.val) {
+        			temp=lists[i];
+        			pos=i;
+        		}
+        	}
+        	p.next=temp;
+        	temp=temp.next;
+        	if (lists[pos]!=null) {
+				
+        		lists[pos]=lists[pos].next;
+			}
+        	if (p!=null) {
+				
+				p=p.next;
+			}
+        }
+        return newHead.next;
     }
 	public static void main(String[] args) {
 		ListNode l1=new ListNode(1);
@@ -67,9 +101,12 @@ public class MergeList {
 		l2.next=l3;
 		l4.next=l5;
 		l5.next=l6;
-		
+		l7.next=l8;
 		MergeList mergeList=new MergeList();
-		System.out.println(mergeList.mergeTwoLists(l1, l4));;
+//		System.out.println(mergeList.mergeTwoLists(l1, l4));;
+		
+		ListNode[] lists={l1,l4,l7};
+		System.out.println(mergeList.mergeKLists(lists));;
 		
 	}
 }
