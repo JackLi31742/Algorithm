e7d89a1683eaf9f8b33335df10ba37a7b57bed23
jackli31742 <317426838@qq.com>
2019-09-02 17:55:41 +0800

    merge list解决

--- src/list/MergeList.java
+++ src/list/MergeList.java
@@ -1,5 +1,8 @@
 package list;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 public class MergeList {
 
 	/**
@@ -60,34 +63,116 @@ public class MergeList {
 			return lists[0];
 		}
         //哨兵
-        ListNode newHead=new ListNode(Integer.MIN_VALUE);
+        ListNode newHead=new ListNode(0);
         ListNode p=newHead;
         while(lists.length>0){
         	int pos=0;
         	ListNode temp=lists[pos];
-        	while(temp==null){
+        	while(temp==null&&pos<lists.length-1){
         		pos++;
         		temp=lists[pos];
         	}
-        	for (int i = 0; i < lists.length; i++) {
-        		if (lists[i].val<temp.val) {
-        			temp=lists[i];
-        			pos=i;
+        	if (temp==null) {
+				break;
+			}
+        	//i从pos开始，减少比较次数
+        	for (int i = pos; i < lists.length; i++) {
+        		if (lists[i]!=null) {
+					
+	        		if (lists[i].val<temp.val) {
+	        			temp=lists[i];
+	        			pos=i;
+	        		}
         		}
         	}
-        	p.next=temp;
-        	temp=temp.next;
-        	if (lists[pos]!=null) {
+//        	if (p!=null) {
+				
+        		p.next=temp;
+//			}
+        	//temp无需走到下一个
+//        	if (temp!=null) {
+//				
+//        		temp=temp.next;
+//			}
+        		
+//        	if (lists[pos]!=null) {
 				
         		lists[pos]=lists[pos].next;
-			}
-        	if (p!=null) {
+//			}
+//        	if (p!=null) {
 				
 				p=p.next;
-			}
+//			}
         }
         return newHead.next;
     }
+	
+	/**
+	 *    利用小顶堆
+	 * @param lists
+	 * @return
+	 */
+	public ListNode mergeKLists2(ListNode[] lists) {
+		if (lists==null||lists.length==0) {
+			return null;
+		}
+		if (lists.length==1) {
+			return lists[0];
+		}
+		Comparator<ListNode> comparator=new Comparator<ListNode>() {
+			public int compare(ListNode node1,ListNode node2) {
+				return Integer.compare(node1.val, node2.val);
+			}
+		};
+		PriorityQueue<ListNode> minHeap=new PriorityQueue<>(lists.length,comparator);
+		
+		
+		for (ListNode listNode : lists) {
+			if (listNode!=null) {
+				
+				minHeap.add(listNode);
+			}
+		}
+		
+		ListNode newHead=new ListNode(0);
+		ListNode p=newHead;
+		while(minHeap.size()>0) {
+			ListNode node=minHeap.poll();
+			p.next=node;
+			p=p.next;
+			//添加下一个比较的节点
+			if(node.next!=null) {
+				minHeap.add(node.next);
+			}
+		}
+		return newHead.next;
+	}
+	
+	/**
+	 *  利用divide and conquer
+	 * @param lists
+	 * @return
+	 */
+	public ListNode mergeKLists3(ListNode[] lists) {
+		if (lists==null||lists.length==0) {
+			return null;
+		}
+		return mergeKLists3(lists,0,lists.length-1);
+	}
+	
+	public ListNode mergeKLists3(ListNode[] lists,int left,int right) {
+		if (left>right) {
+			return null;
+		}
+		if (left==right) {
+			return lists[left];
+		}
+		//移位运算符要加括号
+		int mid=left+((right-left)>>1);
+		ListNode leftList=mergeKLists3(lists,left,mid);
+		ListNode rightList=mergeKLists3(lists,mid+1,right);
+		return mergeTwoLists(leftList, rightList);
+	}
 	public static void main(String[] args) {
 		ListNode l1=new ListNode(1);
 		ListNode l2=new ListNode(4);
@@ -105,8 +190,11 @@ public class MergeList {
 		MergeList mergeList=new MergeList();
 //		System.out.println(mergeList.mergeTwoLists(l1, l4));;
 		
-		ListNode[] lists={l1,l4,l7};
-		System.out.println(mergeList.mergeKLists(lists));;
+		ListNode[] lists={null,l1,null,l4};
+//		System.out.println(mergeList.mergeKLists3(lists));;
+		
+		int mid=2+(3-2)>>1;
+		System.out.println(mid);
 		
 	}
 }
--- src/list/TwoPoint.java
+++ src/list/TwoPoint.java
@@ -144,13 +144,144 @@ public class TwoPoint {
 		return null;
 	}
 	
+	/**
+	 * 19. Remove Nth Node From End of List
+	 * remove the n-th node from the end of list and return its head.
+	 * @param head
+	 * @param n
+	 * @return
+	 */
+	public ListNode removeNthFromEnd(ListNode head, int n) {
+		if (head==null) {
+			return null;
+		}
+		
+        int length=0;
+        ListNode p=head;
+        while(p!=null) {
+        	length++;
+        	p=p.next;
+        }
+        if (length<n) {
+			return head;
+		}
+        if (length==n) {
+        	return head.next;
+		}
+        int dif=length-n;
+        ListNode pre=head;
+        int count=1;
+        //找到要删除节点的前一个节点
+        while(count<dif) {
+        	pre=pre.next;
+        	count++;
+        }
+        //判断要删除的是否是尾节点
+        if (pre.next!=null) {
+			
+        	pre.next=pre.next.next;
+        	//下一个节点已经找不到了，无需再置为null
+//        pre.next=null;
+		}else {
+			pre.next=null;
+		}
+        return head;
+    }
+	/**
+	 * 上题的双指针，时间是一样的
+	 * HP
+	 * @param head
+	 * @param n
+	 * @return
+	 */
+	public ListNode removeNthFromEnd2(ListNode head, int n) {
+		if (head==null) {
+			return head;
+		}
+		ListNode first = head;
+		ListNode second = head;
+		while(n>0&&first!=null) {
+			first=first.next;
+			n--;
+		}
+		//n大于链表的长度
+		if (n>0) {
+			return head;
+		}
+		//n等于链表的长度，也就是要删除头结点
+		if (first==null) {
+			return head.next;
+		}
+		while(first.next!=null) {
+			first=first.next;
+			second=second.next;
+		}
+		if (second.next!=null) {
+			second.next=second.next.next;
+		}else {
+			second.next=null;
+		}
+		return head;
+	}
+	
+	/**
+	 * 上题的双指针，加入哨兵，不用处理头结点，first永远不为null
+	 *
+	 * @param head
+	 * @param n
+	 * @return
+	 */
+	public ListNode removeNthFromEnd3(ListNode head, int n) {
+		if (head==null) {
+			return head;
+		}
+		//哨兵
+		ListNode sentry=new ListNode(0);
+		sentry.next=head;
+		ListNode first = sentry;
+		ListNode second = sentry;
+		while(n>0&&first!=null) {
+			first=first.next;
+			n--;
+		}
+		//n大于链表的长度
+		if (n>0) {
+			return head;
+		}
+		
+		while(first.next!=null) {
+			first=first.next;
+			second=second.next;
+		}
+		if (second.next!=null) {
+			second.next=second.next.next;
+		}else {
+			second.next=null;
+		}
+		return sentry.next;
+	}
+	
+	/**
+	 * 876. Middle of the Linked List
+	 * 如果节点个数是双数，返回第二个节点
+	 */
+	public ListNode middleNode(ListNode head) {
+        ListNode fast=head;
+        ListNode slow =head;
+    }
 	public static void main(String[] args) {
 		ListNode l1=new ListNode(1);
 		ListNode l2=new ListNode(2);
+		ListNode l3=new ListNode(3);
+		ListNode l4=new ListNode(4);
+		ListNode l5=new ListNode(5);
 		l1.next=l2;
-		l2.next=l1;
+		l2.next=l3;
+		l3.next=l4;
+		l4.next=l5;
 		TwoPoint twoPoint=new TwoPoint();
-		System.out.println(twoPoint.detectCycle2(l1));
+//		System.out.println(twoPoint.detectCycle2(l1));
+		System.out.println(twoPoint.removeNthFromEnd3(l1, 2));
 	}
 
 }
